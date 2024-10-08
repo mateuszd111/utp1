58d561b887cbf53a07f1a22aafdf78085379422f
mateuszd111 <s31667@pjwstk.edu.pl>
2024-10-08 16:23:22 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+//TODO: add missing classes
+
 public class Main {
 
     public static void main(String[] args) {
         Adder adder = new Adder();
         System.out.println(adder.add(1, 2));
+        
         Subtractor subtractor = new Subtractor();
         System.out.println(subtractor.subtract(6, 3));
     }
